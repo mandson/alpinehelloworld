45fb6b14c7b501530af81e6f90cd8a88d6985f7f
neutrinos-code <mhamed@yulcom-technologies.com>
2025-01-30 11:27:22 +0000

    correction jenkinsfile 4

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello! Welcome to mandson corp technology'
+    return 'Hello! Welcome to mandson corp technology BUild 2 '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
